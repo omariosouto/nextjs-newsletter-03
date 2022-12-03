efca8b2f2691fcb560bfd9e6fbc6dd3db6bc7ff8
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-12-03 14:30:48 -0300

    A3.1

---
+++ pages/api/newsletter/optin.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const httpStatus = {
+  Success: 200,
+  BadRequest: 400,
+  NotFound: 404,
+  InternalServerError: 500,
+};
+
+const controllerByMethod = {
+  POST(req: NextApiRequest, res: NextApiResponse) { // Cria coisas
+    res
+      .status(httpStatus.Success)
+      .json({ message: "Post request!" });
+  },
+  GET(req: NextApiRequest, res: NextApiResponse) { // Retorna coisas
+    res
+      .status(httpStatus.Success)
+      .json({ message: "Get request!" });
+  }
+}
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const controller = controllerByMethod[request.method];
+  if(!controller) {
+    response
+      .status(httpStatus.NotFound)
+      .json({ message: "Nada encontrado aqui :(" });
+    return;
+  }
+  
+  controller(request, response);
+}
+// export default function handler(
+//   request: NextApiRequest,
+//   response: NextApiResponse
+// ) {
+//   console.log(request.method);
+//   // Servidor
+//   // Request e Response
+//   // Body e Headers
+//   // Status Http (https://httpstatusdogs.com/)
+//   // Request Methods
+//   const responseBody = { name: 'Mario Souto' };
+//   response
+//     .status(httpStatus.Success)
+//     .json(responseBody); // Vira TEXTO Puro pra passar na rede
+// }
